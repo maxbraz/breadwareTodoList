2b52694971e4339ed685518f4b28ef168ab3e3f1
Max Braz <braz.max@gmail.com>
2018-01-03 14:48:15 -0800

    change db path name

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/breadware';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/breadwareTodos';
 
 mongoose.connect(MONGODB_URI, {
   useMongoClient: true,
