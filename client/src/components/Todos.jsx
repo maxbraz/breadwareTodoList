01a6743ad98beb1382aad52c0e304886d2597e9b
Max Braz <braz.max@gmail.com>
2018-01-03 18:00:29 -0800

    change index to stateless component and make Todos stateful.  Allow for redirect from landingPage to Todos screen if login successfully

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import Todo from './Todo.jsx';
 import LogoutBar from './LogoutBar.jsx'
 import {
@@ -10,30 +11,82 @@ import {
   TableHeaderColumn,
 } from 'material-ui/Table';
 
-const Todos = ({todos}) => (
-  <div>
-  <LogoutBar />
-  <Table>
-    <TableHeader
-      displaySelectAll={false}
-      adjustForCheckbox={false}
-      enableSelectAll={false}
-    >
-      <TableRow>
-        <TableHeaderColumn>Title</TableHeaderColumn>
-        <TableHeaderColumn>Summary</TableHeaderColumn>
-        <TableHeaderColumn>Completed</TableHeaderColumn>
-        <TableHeaderColumn></TableHeaderColumn>
-        <TableHeaderColumn></TableHeaderColumn>
-      </TableRow>
-    </TableHeader>
-    <TableBody>
-      {todos.map((todo, i) => {
-        return <Todo key={i} todo={todo} />
-      })}
-    </TableBody>
-  </Table>
-  </div>
-);
+class Todos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    }
+
+    this.addTodo = this.addTodo.bind(this);
+    this.fetchTodos = this.fetchTodos.bind(this);
+  }
+
+    componentDidMount() {
+    this.fetchTodos();
+  }
+
+  addTodo(todo) {
+    let todos;
+    axios.post('/todo', {
+      todo: todo
+    })
+    .then(function(response) {
+      todos = response.data;
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+
+    this.fetchTodos(todos);
+  }
+
+  fetchTodos(todos) {
+    let fetchedTodos;
+    let that = this;
+
+    axios.get('/todos', {
+      params: {
+        todos: todos
+      }
+    })
+    .then(function (response) {
+      fetchedTodos = response.data;
+
+      that.setState({todos: fetchedTodos});
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  render () {
+    return (
+      <div>
+      <LogoutBar />
+      <Table>
+        <TableHeader
+          displaySelectAll={false}
+          adjustForCheckbox={false}
+          enableSelectAll={false}
+        >
+          <TableRow>
+            <TableHeaderColumn>Title</TableHeaderColumn>
+            <TableHeaderColumn>Summary</TableHeaderColumn>
+            <TableHeaderColumn>Completed</TableHeaderColumn>
+            <TableHeaderColumn></TableHeaderColumn>
+            <TableHeaderColumn></TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {this.state.todos.map((todo, i) => {
+            return <Todo key={i} todo={todo} />
+          })}
+        </TableBody>
+      </Table>
+      </div>
+    )
+  }
+}
 
 export default Todos;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
+import LandingPage from './components/LandingPage.jsx';
 import Todos from './components/Todos.jsx';
 import testData from '../../data.json';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -10,66 +11,15 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: []
-    }
-
-    this.addTodo = this.addTodo.bind(this);
-    this.fetchTodos = this.fetchTodos.bind(this);
-  }
-
-  componentDidMount() {
-    this.fetchTodos();
-  }
-
-  addTodo(todo) {
-    let todos;
-    axios.post('/todo', {
-      todo: todo
-    })
-    .then(function(response) {
-      todos = response.data;
-    })
-    .catch(function(error) {
-      console.log(error);
-    })
-
-    this.fetchTodos(todos);
-  }
-
-  fetchTodos(todos) {
-    let fetchedTodos;
-    let that = this;
-
-    axios.get('/todos', {
-      params: {
-        todos: todos
-      }
-    })
-    .then(function (response) {
-      fetchedTodos = response.data;
-
-      that.setState({todos: fetchedTodos});
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-
-  render () {
-    return (
-      <MuiThemeProvider>
-        <Router>
-          <div>
-            <Todos todos={this.state.todos}/>
-          </div>
-        </Router>
-      </MuiThemeProvider>
-    )
-  }
-}
+const App = () => (
+  <MuiThemeProvider>
+    <Router>
+      <div>
+        <LandingPage />
+        <Route path='/todos' component={Todos} />
+      </div>
+    </Router>
+  </MuiThemeProvider>
+)
 
 ReactDOM.render(<App />, document.getElementById('app'));
