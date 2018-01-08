b15170ac0deffe366dd6d2d91fa3f6c8fd135e5c
Max Braz <braz.max@gmail.com>
2018-01-07 22:55:57 -0800

    add class transform

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
+    publicPath: '/'
   },
   module: {
     loaders: [
@@ -15,9 +16,19 @@ module.exports = {
         include: SRC_DIR,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015'],
+          plugins: ['transform-class-properties']
         }
       }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  resolve: {
+    modules: [
+      path.resolve('./client'),
+      'node_modules'
     ]
   }
 };
