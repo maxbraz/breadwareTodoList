287167c103cb856a800ee9694583574eb84e2c38
Max Braz <braz.max@gmail.com>
2018-01-07 22:56:41 -0800

    use hashRouter.  fixed static page issue

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import store from './store';
 import LandingPage from './components/LandingPage.jsx';
 import Todos from './components/Todos.jsx';
